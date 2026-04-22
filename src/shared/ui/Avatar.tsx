def59ea9f752f6b375bb1151e790185c4e6b7769
heeyoung123 <heeyoung321@naver.com>
2026-04-23 00:52:22 +0900

    feat: 뱃지 컴포넌트 사이즈 변수 설정

--- src/shared/ui/Avatar.tsx
+++ src/shared/ui/Avatar.tsx
@@ -2,17 +2,31 @@
 
 import * as React from 'react'
 import * as AvatarPrimitive from '@radix-ui/react-avatar'
+import { tv, type VariantProps } from 'tailwind-variants'
 import { cn } from '@/shared/lib/Cn'
+
+const avatarVariants = tv({
+  base: 'relative flex shrink-0 overflow-hidden rounded-full',
+  variants: {
+    size: {
+      sm: 'size-[2.3125rem]',
+      lg: 'h-[7.3125rem] w-[7.4375rem]',
+      xl: 'h-[8.9375rem] w-[9.1875rem]',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+})
+
+type AvatarProps = React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root> &
+  VariantProps<typeof avatarVariants>
 
 export const Avatar = React.forwardRef<
   React.ComponentRef<typeof AvatarPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof AvatarPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <AvatarPrimitive.Root
-    ref={ref}
-    className={cn('relative flex size-10 shrink-0 overflow-hidden rounded-full', className)}
-    {...props}
-  />
+  AvatarProps
+>(({ className, size, ...props }, ref) => (
+  <AvatarPrimitive.Root ref={ref} className={cn(avatarVariants({ size }), className)} {...props} />
 ))
 Avatar.displayName = AvatarPrimitive.Root.displayName
 
